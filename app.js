8cab3223e8371c649e9fb4d6d6c2bf0e25364beb
amer <amermostaafa@gmail.com>
2021-02-08 22:30:34 -0600

    edit app.listen function

--- app.js
+++ app.js
@@ -175,6 +175,6 @@ req.login(user, function(err){
 
 
 
-app.listen(3000, function(){
-  console.log("server is running on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("ok");
 })
